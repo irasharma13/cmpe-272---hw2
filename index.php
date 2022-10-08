65dbfff5f6434d5fec614c2f16cb8678048ebeaa
irasharma13 <irasharma13@gmail.com>
2022-10-08 06:23:35 -0700

    uploading hw2

---
+++ index.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>CMPE 272 - Real Estate</title>
+       
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+       
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+       
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+
+        <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand" href="#page-top">CASAREAL</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars ms-1"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+                        <li class="nav-item"><a class="nav-link" href="#services">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#portfolio">Products</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#about">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#team">News</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#contact">Contacts</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <header class="masthead">
+            <div class="container">
+                <div class="masthead-subheading">Find your perfect Home!</div>
+                <div class="masthead-heading text-uppercase">Explore homes</div>
+                
+            </div>
+        </header>
+
+        <section class="page-section" id="services">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">Products</h2>
+                    <h3 class="section-subheading text-muted">Buy, Sell, Rent</h3>
+                </div>
+                <div class="row text-center">
+                    <div class="col-md-4">
+                        <span class="fa-stack fa-4x">
+                            <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                            <i class="fas fa-shopping-cart fa-stack-1x fa-inverse"></i>
+                        </span>
+                        <h4 class="my-3">Buy</h4>
+                        <p class="text-muted">Extensive database to look at when exploring options fo buying. Search for homes or apartments based on your needs. Connect with agents and describe your needs. </p>
+                    </div>
+                    <div class="col-md-4">
+                        <span class="fa-stack fa-4x">
+                            <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                            <i class="fas fa-business-time fa-stack-1x fa-inverse"></i>
+                        </span>
+                        <h4 class="my-3">Sell</h4>
+                        <p class="text-muted">Sell your home at Casareal! Connect with agents and discuss the costs of service and increase the cost of the home including staging, painting, renovations. No payment is collected till the home is sold!</p>
+                    </div>
+                    <div class="col-md-4">
+                        <span class="fa-stack fa-4x">
+                            <i class="fas fa-circle fa-stack-2x text-primary"></i>
+                            <i class="fas fa-key fa-stack-1x fa-inverse"></i>
+                        </span>
+                        <h4 class="my-3">Rent</h4>
+                        <p class="text-muted">Sorting criteria on the platform for the users to rent apartments or houses for short or long term lease. Background check donw in advance to ensure there's no spam or fraud. </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+       
+     
+        <section class="page-section" id="about">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">About</h2>
+                    <h3 class="section-subheading text-muted"></h3>
+                </div>
+                <ul class="timeline">
+                    <li>
+                        <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/home-about1.jpg" alt="..." /></div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                                
+                                <h4 class="subheading">Agency Road Map</h4>
+                            </div>
+                            <div class="timeline-body"><p class="text-muted">Started in 2022, CasaReal is a real estate platform, pairing the industry’s top talent with technology to make the search for renting, buying and selling properties</p></div>
+                        </div>
+                    </li>
+                   <li class="timeline-inverted">
+                        <div class="timeline-image"><img class="rounded-circle img-fluid" src="assets/img/about/home-about2.jpg" alt="..." /></div>
+                        <div class="timeline-panel">
+                            <div class="timeline-heading">
+                               
+                                <h4 class="subheading">Company reinventing the space</h4>
+                            </div>
+                            <div class="timeline-body"><p class="text-muted">To lead the industry requires the smartest tools built by the brightest minds across engineering, design, and strategy. Through our proprietary platform, Compass is changing how agents and clients navigate the process of finding or selling a home.</p></div>
+                        </div>
+                    </li>
+                    
+                    <li class="timeline-inverted">
+                        <div class="timeline-image">
+                            <h4>
+                                Be Part
+                                <br />
+                                Of Our
+                                <br />
+                                Story!
+                            </h4>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <!-- Team-->
+        <section class="page-section bg-light" id="team">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">News</h2>
+                    <h3 class="section-subheading text-muted">Explore latest homes avaibale in mentioned cities</h3>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4">
+                        <div class="team-member">
+                            <img class="mx-auto rounded-circle" src="assets/img/news/news1.jpg" alt="..." />
+                            <h4>San Francisco</h4>
+                            <p class="text-muted">4bd|3b (2,702 sqft) for $1,995,000</p>
+                           
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="team-member">
+                            <img class="mx-auto rounded-circle" src="assets/img/news/news2.jpg" alt="..." />
+                            <h4>New York</h4>
+                            <p class="text-muted">3bd|2ba (1,536 sqft) for $1,750,000</p>
+                            
+                        </div>
+                    </div>
+                    <div class="col-lg-4">
+                        <div class="team-member">
+                            <img class="mx-auto rounded-circle" src="assets/img/news/news3.jpg" alt="..." />
+                            <h4>Florida</h4>
+                            <p class="text-muted">4bd|4ba (2,777 sqft) for $675,000</p>
+                            
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-8 mx-auto text-center"><p class="large text-muted">Contact us for more information </div>
+                </div>
+            </div>
+        </section>
+        <?php  
+
+        $myFile = 'contact.txt';
+        $data = [];
+        $myFileContent = fopen($myFile, 'r');
+        $theData = fread($myFileContent, 0);
+        while (!feof($myFileContent)) {
+            $data[] = fgets($myFileContent);
+        }
+       
+        fclose($myFileContent);
+        echo $theData;
+        ?>
+        <div class="py-5">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-md-3 col-sm-6 my-3">
+
+                        <div class="btn btn-dark btn-social mx-2"  aria-label="Larry Parker Twitter Profile"><i class="fas fa-user-alt"></i></div>
+                        
+                        <?php echo ($data[0]); ?>
+
+                    </div>
+                    <div class="col-md-3 col-sm-6 my-3">
+                        <div class="btn btn-dark btn-social mx-2" href="#!" aria-label="Larry Parker Twitter Profile"><i class="fas fa-envelope-open-text"></i></div>
+                        
+                        <?php echo ($data[1]); ?>
+  
+                    </div>
+                    <div class="col-md-3 col-sm-6 my-3">
+                        <div class="btn btn-dark btn-social mx-2" href="#!" aria-label="Larry Parker Twitter Profile"><i class="fas fa-phone-alt"></i></div>
+                        
+                        <?php echo ($data[2]); ?>
+                    </div>
+                    <div class="col-md-3 col-sm-6 my-3">
+                        <div class="btn btn-dark btn-social mx-2" href="#!" aria-label="Larry Parker Twitter Profile"><i class="fas fa-house-user"></i></div>
+                       
+                        <?php echo ($data[3]); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <!-- Contact-->
+        <section class="page-section" id="contact">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">Contact Us</h2>
+                    <h3 class="section-subheading text-muted">Please fill the form</h3>
+                </div>
+              
+                <form id="contactForm" data-sb-form-api-token="API_TOKEN">
+                    <div class="row align-items-stretch mb-5">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <!-- Name input-->
+                                <input class="form-control" id="name" type="text" placeholder="Your Name *" data-sb-validations="required" />
+                                <div class="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
+                            </div>
+                            <div class="form-group">
+                                <!-- Email address input-->
+                                <input class="form-control" id="email" type="email" placeholder="Your Email *" data-sb-validations="required,email" />
+                                <div class="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>
+                                <div class="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>
+                            </div>
+                            <div class="form-group mb-md-0">
+                                <!-- Phone number input-->
+                                <input class="form-control" id="phone" type="tel" placeholder="Your Phone *" data-sb-validations="required" />
+                                <div class="invalid-feedback" data-sb-feedback="phone:required">A phone number is required.</div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group form-group-textarea mb-md-0">
+                                <!-- Message input-->
+                                <textarea class="form-control" id="message" placeholder="Your Message *" data-sb-validations="required"></textarea>
+                                <div class="invalid-feedback" data-sb-feedback="message:required">A message is required.</div>
+                            </div>
+                        </div>
+                    </div>
+                   
+                    
+                    
+                </form>
+            </div>
+        </section>
+        <!-- Footer-->
+        
+        
+        
+        
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
+</html>
